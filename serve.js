f63b773e65056e19ef60d4510a8218a300d18202
Andre Derme <andredermee@gmail.com>
2025-10-12 16:34:03 -0300

    Update serve.js

--- serve.js
+++ serve.js
@@ -1,6 +1,37 @@
-import express from "express"
+import express, { json, request, response } from "express"
 
 const app = express ()
 
 
-app.get("/usuarios")
+app.use(express.json())
+
+
+const users = []
+
+
+app.post ("/usuarios", (request, response) =>{
+
+    users.push (request.body)
+
+    response.status(201).json(request.body)
+
+})
+
+app.get("/usuarios", (request, response) => {
+    
+    response.status(200).json(users)
+
+})
+
+app.listen (3000)
+
+/*            missão
+    criar uma api para os usuarios
+
+    -criar um usuario
+    -listar todos 
+    -editar um usuario
+    -deletar um usuario
+
+
+*/ 
