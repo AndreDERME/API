916b9ed458bcb596933d3b63961a038209e81111
Andre Derme <andredermee@gmail.com>
2025-10-15 20:23:05 -0300

    Update serve.js

--- serve.js
+++ serve.js
@@ -28,8 +28,23 @@ app.post ('/usuarios', async (request, response) =>{
 })
 
 app.get('/usuarios', async (request, response) => {
+    let users = []
 
-   const users = await prisma.user.findMany()
+    if (request.query){
+        users = await prisma.user.findMany({
+            where:{
+                name: request.query.name,
+                email: request.query.email,
+                age: request.query.age
+
+            }
+        })
+    }
+    else{
+        users = await prisma.user.findMany()
+    }
+
+  
 
     response.status(200).json(users)
 
@@ -58,7 +73,7 @@ app.delete('/usuarios/:id', async (request, response)=>{
         where:{
             id: request.params.id,
         },
-    f})
+    })
 
     response.status(201).json({message: "usuario deletado com sucesso!"})
 
